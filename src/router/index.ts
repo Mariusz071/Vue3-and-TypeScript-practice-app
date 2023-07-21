1ed25873d6dad5934010044de757a70bcad04727
Mariusz <siewruk.mariusz92@gmail.com>
2023-07-21 18:28:16 +0200

    Changed router export

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AuthView from '@/views/AuthView.vue'
 import SignIn from '@/modules/Auth/components/SignIn'
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -25,8 +25,12 @@ const router = createRouter({
           component: () => import('@/modules/Auth/components/CreateAccount')
         }
       ]
+    },
+    {
+      name: 'u.suppliers',
+      path: '/suppliers',
+      children: [],
+      component: () => import('@/views/SuppliersView')
     }
   ]
 })
-
-export default router
