18b4434f9223fc2a5efa2e1df2fb1d091cb3b092
Mariusz <siewruk.mariusz92@gmail.com>
2023-07-22 18:37:13 +0200

    Added nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import Alert from '@/modules/Alert'
+import { ref, computed, watch } from 'vue'
 import { RouterView } from 'vue-router'
+import Alert from '@/modules/Alert'
 import Container from '@/components/Container.vue'
+import MainNav from '@/components/MainNav.vue'
 
 import { useAuthStore } from '@/stores/auth'
 import { TOKEN_STORAGE_KEY } from '@/common/constants'
@@ -10,21 +11,26 @@ import { TOKEN_STORAGE_KEY } from '@/common/constants'
 //store
 const authStore = useAuthStore()
 const hasAuthToken = computed(() => !!authStore.authToken)
+
 ///
+
+// nav
+const isOpen = ref('false')
+///
+
+//
 </script>
 
 <template lang="pug">
 v-app
   v-layout
     v-app-bar(title="Opply Task")
-      template(
-        v-if="hasAuthToken"
-        #append
-      )
-        v-btn(
-          color="secondary"
-          @click="authStore.onLogout"
-        ) Logout
+      template(#append)
+        | Random text
+    MainNav(
+      v-if="hasAuthToken"
+      v-model="isOpen"
+    )
     v-main
       Alert
       Container
---
+++ src/components/MainNav.vue
@@ -0,0 +1,59 @@
+<template lang="pug">
+v-navigation-drawer(
+  v-model="isOpen"
+  v-bind="{ location }"
+  app
+  dark
+)
+  v-list.pt-0
+    v-list-item(
+      v-for="item in navItems"
+      :key="item.path"
+      :active="item.isActive"
+      :to="item.path"
+      link
+    )
+      v-icon.mr-2 {{ item.icon }}
+      | {{ item.label }}
+  template(#append)
+    v-divider.mt-4
+    v-list-item(
+      @click="authStore.onLogout"
+    ) 
+      v-icon.mr-2 fa-solid fa-arrow-right-from-bracket
+      | Logout
+      
+  </template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useDisplay } from 'vuetify'
+import { useAuthStore } from '@/stores/auth'
+
+// navigation
+const route = useRoute()
+const { xs } = useDisplay()
+const isOpen = ref(false)
+const location = computed(() => (xs.value ? 'bottom' : 'start'))
+
+const navItems = computed(() => [
+  {
+    icon: 'fas fa-truck-field',
+    label: 'Suppliers',
+    path: '/suppliers',
+    isActive: route.name === 'u.suppliers'
+  },
+  {
+    icon: 'fas fa-quote-left',
+    label: 'Quotes',
+    path: '/quotes',
+    isActive: route.name === 'u.quotes'
+  }
+])
+///
+
+// store
+const authStore = useAuthStore()
+///
+</script>
