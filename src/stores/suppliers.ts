1da6f861e769637065db2f6e8a7643300de545f3
Mariusz <siewruk.mariusz92@gmail.com>
2023-07-22 10:34:08 +0200

    Cleaned up suppliers store

--- src/stores/suppliers.ts
+++ src/stores/suppliers.ts
@@ -1,24 +1,32 @@
-import { reactive, ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+
 import { getSuppliers } from '@/views/SuppliersView/api'
 import { useAlert } from '@/stores/alert'
 
-export const useSuppliersStore = defineStore('suppliers', () => {
-  const suppliers = ref({})
+import type { Ref } from 'vue'
+import type { Supplier } from '@/views/SuppliersView/types'
 
-  const getSuppliersAction = async () => {
+export const useSuppliersStore = defineStore('suppliers', () => {
+  const itemsPerPage = 10
+  const suppliers: Ref<Supplier[]> = ref([])
+  const count: Ref<number> = ref(0)
+  const totalPages = computed(() => Math.ceil(count.value / itemsPerPage))
+
+  const getSuppliersAction = async (params?: object) => {
     const alertsStore = useAlert()
 
     try {
-      const res = await getSuppliers()
-      suppliers.value = res
+      const res = await getSuppliers(params)
+      suppliers.value = res.results
+      count.value = res.count
     } catch (e: any) {
       alertsStore.show({
         type: 'error',
-        text: `Please log in first. Error: ${e.message}.`
+        text: `Failed to fetch suppliers list. Error: ${e.message}.`
       })
     }
   }
 
-  return { suppliers, getSuppliersAction }
+  return { suppliers, count, getSuppliersAction, totalPages }
 })
